3e58b34e5aa8117f82569022f4b23bc3cbf6e7fe
TranQuy <dtquy2510@gmail.com>
2026-05-26 17:48:49 +0700

    add Controller + ConnectDB

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,11 +1,17 @@
 <?php
-class home{
-    public function index(){
-        require_once '../app/view/home/index.php';
-    }
-    public function login(){
-        require_once '../app/view/home/login.php';
-    }
-}
+class home
+{
+  public function index()
+  {
+    echo "Đây là trang chủ";
+    
+  }
 
-?>
+  public function about()
+  {
+    echo "Đây là trang giới thiệu";
+  }
+  public function login(){
+   require_once'../app/views/home/login.php'; 
+  }
+}
